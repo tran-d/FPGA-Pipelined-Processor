ba1a9bc033ce7231cbb34d32dd3f8138d6e04395
David Tran <dht9@duke.edu>
2018-02-28 21:51:04 -0500

    Added insn controls

--- controls.v
+++ controls.v
@@ -1,17 +1,52 @@
 /***************************************************************************************************************/
 
-module controls(opcode, ALU_op, RWE);
+module controls(opcode, ALU_op, Rwe);
 
 	input [4:0] opcode, ALU_op;
 	output RWE;
-
+	
+	wire add, addi, sub, and_insn, or_insn, sll, sra, mul, div, sw, lw, j, bne, jal, jr, blt, bex, setx, custom_r;
+	wire r_insn;
+	wire ALU_add, ALU_sub, ALU_and, ALU_or, ALU_sll, ALU_sra, ALU_mul, ALU_div;
+	
+	assign r_insn 		= ~opcode[4] & ~opcode[3] & ~opcode[2] & ~opcode[1] & ~opcode[0];
+	
+	assign ALU_add 	= ~ALU_op[4] & ~ALU_op[3] & ~ALU_op[2] & ~ALU_op[1] & ~ALU_op[0];	//00000
+	assign ALU_sub 	= ~ALU_op[4] & ~ALU_op[3] & ~ALU_op[2] & ~ALU_op[1] &  ALU_op[0];	//00001
+	assign ALU_and 	= ~ALU_op[4] & ~ALU_op[3] & ~ALU_op[2] &  ALU_op[1] & ~ALU_op[0];	//00010
+	assign ALU_or	 	= ~ALU_op[4] & ~ALU_op[3] & ~ALU_op[2] &  ALU_op[1] &  ALU_op[0];	//00011
+	assign ALU_sll 	= ~ALU_op[4] & ~ALU_op[3] &  ALU_op[2] & ~ALU_op[1] & ~ALU_op[0];	//00100
+	assign ALU_sra 	= ~ALU_op[4] & ~ALU_op[3] &  ALU_op[2] & ~ALU_op[1] &  ALU_op[0];	//00101
+	assign ALU_mul 	= ~ALU_op[4] & ~ALU_op[3] &  ALU_op[2] &  ALU_op[1] & ~ALU_op[0];	//00110
+	assign ALU_div 	= ~ALU_op[4] & ~ALU_op[3] &  ALU_op[2] &  ALU_op[1] &  ALU_op[0];	//00111
+	
+	
+	assign add 			= r_insn && ALU_add;
+	assign addi 		= ~opcode[4] & ~opcode[3] &  opcode[2] & ~opcode[1] &  opcode[0];	//00101
+	assign sub 			= r_insn && ALU_sub;
+	assign and_insn 	= r_insn && ALU_and;
+	assign or_insn 	= r_insn && ALU_or;
+	assign sll 			= r_insn && ALU_sll;
+	assign sra 			= r_insn && ALU_sra;
+	assign mul 			= r_insn && ALU_mul;
+	assign div 			= r_insn && ALU_div;
+	
+	assign sw			= ~opcode[4] & ~opcode[3] &  opcode[2] &  opcode[1] &  opcode[0];	//00111
+	assign lw	 		= ~opcode[4] &  opcode[3] & ~opcode[2] & ~opcode[1] & ~opcode[0];	//01000
+	
+	assign j		 		= ~opcode[4] & ~opcode[3] & ~opcode[2] & ~opcode[1] &  opcode[0];	//00001
+	assign bne	 		= ~opcode[4] & ~opcode[3] & ~opcode[2] &  opcode[1] & ~opcode[0];	//00010
+	assign jal	 		= ~opcode[4] & ~opcode[3] & ~opcode[2] &  opcode[1] &  opcode[0];	//00011
+	assign jr			= ~opcode[4] & ~opcode[3] &  opcode[2] & ~opcode[1] & ~opcode[0];	//00100
+	assign blt	 		= ~opcode[4] & ~opcode[3] &  opcode[2] &  opcode[1] & ~opcode[0];	//00110
+	
+	assign bex			=  opcode[4] & ~opcode[3] &  opcode[2] &  opcode[1] & ~opcode[0];	//10110
+	assign setx			=  opcode[4] & ~opcode[3] &  opcode[2] & ~opcode[1] &  opcode[0];	//10101
+//	assign custom_r 	=  opcode[3] & (opcode[4] || opcode[2] & ; 								//01000 - 11111
+	
+	
 	/* write to regfile */
-	assign RWE = (~opcode[4] & ~opcode[3] & ~opcode[2] & ~opcode[1] & ~opcode[0]) || //R insn
-					(~opcode[4] & ~opcode[3] &  opcode[2] & ~opcode[1] &  opcode[0]) || //addi
-					(~opcode[4] &  opcode[3] & ~opcode[2] & ~opcode[1] & ~opcode[0]) || //lw
-					(~opcode[4] & ~opcode[3] & ~opcode[2] &  opcode[1] &  opcode[0]) || //jal T
-					( opcode[4] & ~opcode[3] &  opcode[2] & ~opcode[1] &  opcode[0]) || //setx T
-					(opcode[3]);																		  //custom_r
+	assign Rwe = r_insn || addi || lw || jal || setx || custom_r;
 	
 	
 endmodule
