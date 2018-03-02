547ffd22f3639bc58fd1df0ba90f37f07e0703ec
David Tran <dht9@duke.edu>
2018-03-02 17:25:32 -0500

    working on insn branching

--- pc_module.v
+++ pc_module.v
@@ -1,16 +1,21 @@
-module pc_module(pc_in, clock, reset, pc_ena, pc_out, pc_plus_1, pc_upper_5);
+module pc_module(pc_in, clock, reset, pc_ena, address_imem, pc_plus_4, pc_upper_5);
 
 	input [31:0] pc_in;
 	input clock, reset, pc_ena;
-	output [31:0] pc_out, pc_plus_1;
+	output [31:0] pc_plus_4;
+	output [11:0] address_imem;
 	output [4:0] pc_upper_5;
 	
+
+	wire [31:0] pc_out;
+	
+	assign address_imem[11:0] = pc_out[11:0];
 	
 	wire dovf, dne, dlt;
 	
 	PC my_PC(pc_in, clock, reset, pc_ena, pc_out);
 	
-	adder32 my_adder32(pc_in, {{29{1'b0}}, 1'b1, 2'b00}, 1'b0, pc_plus_1, dovf, dne, dlt);
+	adder32 my_adder32(pc_out, {{29{1'b0}}, 1'b1, 2'b00}, 1'b0, pc_plus_4, dovf, dne, dlt);
 
 	assign pc_upper_5[4:0] = pc_out[31:27];
 	
